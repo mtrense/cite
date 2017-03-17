a5d604766fbca6d560a883518196810bc48a1320
Max Trense <dev@trense.info>
2017-03-17 11:35:52 +0100

    Splits builder-methods to create template from file/inline

--- lib/cite.rb
+++ lib/cite.rb
@@ -6,8 +6,14 @@ module Cite
   
   class <<self
     
-    def Template(source, options = {})
-      Template.new source, options
+    def file(source, options = {})
+      Template.new source, options.merge(file: source, line: 1)
+    end
+    
+    def inline(source, file, *line_and_options)
+      options = Hash === line_and_options.last ? line_and_options.pop : {}
+      line = Integer === line_and_options.first ? line_and_options.shift : 1
+      Template.new source, options.merge(file: file, line: line)
     end
     
   end
--- lib/cite/template.rb
+++ lib/cite/template.rb
@@ -1,10 +1,34 @@
+# encoding: UTF-8
+# Copyright 2016 Max Trense
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'pathname'
 
 module Cite
   
   class Template
     
     def initialize(source, options = {})
-      @source = Pathname(source)
+      if options.include?(:filename)
+        @file = options.delete(:file)
+        @line = options.delete(:line)
+        @source = source
+      else
+        @file = source.to_s
+        @line = 1
+        @source = File.read(@file)
+      end
       @stream_name = options.delete(:stream_name) || '__citeout'
       @correct_indentation = options.delete(:correct_indentation) || true
       @options = options
@@ -15,7 +39,7 @@ module Cite
     def assembled_template
       @assembled_template ||= begin
         template = ''
-        analyzer = Analyzer.new(Tokenizer.new(@source.read))
+        analyzer = Analyzer.new(Tokenizer.new(@source))
         indentation = nil
         state = :text
         analyzer.each do |type, token|
@@ -54,14 +78,19 @@ module Cite
     end
     
     def render(*args)
-      locals = Hash === args.last ? args.pop : {}
-      object = args.first || Object.new
-      p = eval <<-EOF, binding, @source.to_s, -1
-        proc do |#{locals.keys.join(', ')}|
-          #{assembled_template}
-        end
-      EOF
-      object.instance_exec *locals.values, &p
+      begin
+        locals = Hash === args.last ? args.pop : {}
+        object = args.first || Object.new
+        (Thread[:cite_templates] ||= []) << [self, object, locals]
+        p = eval <<-EOF, binding, @source.to_s, -1
+          proc do |#{locals.keys.join(', ')}|
+            #{assembled_template}
+          end
+        EOF
+        object.instance_exec *locals.values, &p
+      ensure
+        Thread[:cite_templates].pop
+      end
     end
     
   end
